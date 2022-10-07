030e29371c87216e427b993df6fe391bced8389b
Ivan-Samoylenko <ivansamoylenko@ukr.net>
2022-10-07 18:45:09 +0300

    add solved task two

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import userProfile from '../user.json';
+import statisticData from '../data.json';
 
 export const App = () => {
   return (
     <>
       <Profile profile={userProfile} />
+      <Statistics data={statisticData} title="Upload stats" />
     </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,44 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ data, title }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+
+      <ul className={css['stat-list']}>
+        {data.map(dataItem => {
+          return (
+            <li
+              className={css.item}
+              key={dataItem.id}
+              style={{ backgroundColor: getBgColor() }}
+            >
+              <span className={css.label}>{dataItem.label}</span>
+              <span className={css.percentage}>{dataItem.percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+  title: PropTypes.string,
+};
+
+function getBgColor() {
+  return `rgb(${50 + Math.floor(Math.random() * 91)}, ${
+    50 + Math.floor(Math.random() * 91)
+  }, ${50 + Math.floor(Math.random() * 91)})`;
+}
+
+export default Statistics;
